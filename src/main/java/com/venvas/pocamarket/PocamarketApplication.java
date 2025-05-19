aec85ccad261dceddda83f2063ee608cfc740dc3
Taeuk Kang <rkdxodnr07@gmail.com>
2025-05-19 15:25:24 +0900

    first commit
    start springboot project

---
+++ src/main/java/com/venvas/pocamarket/PocamarketApplication.java
@@ -0,0 +1,13 @@
+package com.venvas.pocamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocamarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocamarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venvas/pocamarket/PocamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venvas.pocamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocamarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
