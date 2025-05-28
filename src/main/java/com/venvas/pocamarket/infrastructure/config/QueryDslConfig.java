f5359fec357093d2c6eaf3114ccfe6d848b8c826
sayarn <82561899+yunyuseok@users.noreply.github.com>
2025-05-28 18:58:42 +0900

    Add: queryDsl에서 사용 Hibernate 버전 6.x -> 5.x
    queryDsl에서 사용하는 transform 메서드 관련 이슈 수정 사항.
    현재 프로젝트 Hibernate는 6.x
    queryDsl은 Hibernate 5.x 에서 만들어져서
    그냥 사용 시, 6.x에 없는 메서드 호출로 에러

---
+++ src/main/java/com/venvas/pocamarket/infrastructure/config/QueryDslConfig.java
@@ -0,0 +1,30 @@
+package com.venvas.pocamarket.infrastructure.config;
+
+import com.querydsl.jpa.DefaultQueryHandler;
+import com.querydsl.jpa.Hibernate5Templates;
+import com.querydsl.jpa.QueryHandler;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(new CustomHibernate5Templates(), entityManager);
+    }
+
+    static public class CustomHibernate5Templates extends Hibernate5Templates {
+        @Override
+        public QueryHandler getQueryHandler() {
+            return DefaultQueryHandler.DEFAULT;
+        }
+    }
+}
