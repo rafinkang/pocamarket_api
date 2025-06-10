e1a828dd2d10a94355ea93f22ff9b97ee304285f
sayarn <82561899+yunyuseok@users.noreply.github.com>
2025-06-10 17:41:44 +0900

    fix: orderList rarity 제거

--- src/main/java/com/venvas/pocamarket/service/pokemon/domain/value/UseOrder.java
+++ src/main/java/com/venvas/pocamarket/service/pokemon/domain/value/UseOrder.java
@@ -4,7 +4,7 @@ package com.venvas.pocamarket.service.pokemon.domain.value;
 import java.util.List;
 
 public final class UseOrder {
-    private static final List<String> list = List.of("code", "nameKo", "rarity");
+    private static final List<String> list = List.of("code", "nameKo");
 
     public static List<String> getList() {
         return list;
