930d704c3fc7c46a421510b5be9c9dd1eca4c2be
Taeuk Kang <rkdxodnr07@gmail.com>
2025-06-22 19:58:47 +0900

    Update UserUpdateRequest.java

--- src/main/java/com/venvas/pocamarket/service/user/application/dto/UserUpdateRequest.java
+++ src/main/java/com/venvas/pocamarket/service/user/application/dto/UserUpdateRequest.java
@@ -31,7 +31,7 @@ public class UserUpdateRequest {
     // 비밀번호 변경을 위한 필드
     private String currentPassword;
     
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", 
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$", 
              message = "비밀번호는 최소 8자 이상이며, 영문, 숫자, 특수문자를 각각 1개 이상 포함해야 합니다")
     private String newPassword;
 
