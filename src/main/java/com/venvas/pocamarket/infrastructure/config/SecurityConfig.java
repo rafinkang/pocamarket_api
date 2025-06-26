75b5c4d30ae8a96ff3c8a66fa28256b835ebc800
Taeuk Kang <rkdxodnr07@gmail.com>
2025-06-26 20:19:16 +0900

    Update SecurityConfig.java

--- src/main/java/com/venvas/pocamarket/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/venvas/pocamarket/infrastructure/config/SecurityConfig.java
@@ -148,6 +148,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 프론트엔드 출처
+        configuration.addAllowedOrigin("http://localhost"); // 허용할 프론트엔드 출처
+        configuration.addAllowedOrigin("http://3.34.124.198"); // 허용할 프론트엔드 출처
+        configuration.addAllowedOrigin("http://ec2-54-252-151-78.ap-southeast-2.compute.amazonaws.com"); // 허용할 프론트엔드 출처
         configuration.addAllowedMethod("GET"); // 허용할 HTTP 메서드들을 개별적으로 설정
         configuration.addAllowedMethod("POST");
         configuration.addAllowedMethod("PUT");
